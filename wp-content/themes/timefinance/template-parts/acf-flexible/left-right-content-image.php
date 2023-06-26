ecee092ec35a7aca68f1a6212a6710ccbb3f6ec4
rajlinksture <116059894+rajlinksture@users.noreply.github.com>
2023-06-26 11:56:32 +0530

    Some changes

--- wp-content/themes/timefinance/template-parts/acf-flexible/left-right-content-image.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/left-right-content-image.php
@@ -19,7 +19,7 @@ $image_alt               = ( isset( $image['alt'] ) && ! empty( $image['alt'] ) 
 ?>
 <section class="left-right-content-image <?php echo $select_background_color; ?>" id="<?php echo $section_id; //phpcs:ignore ?>">
 	<div class="container">
-		<div class="row<?php echo $reverse; //phpcs:ignore ?>">
+		<div class="row justify-content-center align-items-center<?php echo $reverse; //phpcs:ignore ?>">
 			<div class="col-6">
 				<img class="left-right-image" width="<?php echo $image['sizes']['timefinance-desktop-width']; ?>" height="<?php echo 	$image['sizes']['timefinance-desktop-height']; ?>" src="<?php echo $image['url']; ?>" srcset="<?php echo $image['sizes']['timefinance-small-mobile']; ?> 400w, <?php echo $image['sizes']['timefinance-mobile']; ?> 800w, <?php echo $image['sizes']['timefinance-tablet']; ?> 1200w, <?php echo $image['sizes']['timefinance-desktop']; ?> 2000w" sizes="50vw" alt="<?php echo $image_alt; //phpcs:ignore ?>">
 			</div>
