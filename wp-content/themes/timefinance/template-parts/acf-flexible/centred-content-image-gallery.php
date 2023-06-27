dfefb77d7c3f7fd9e009b9be3b712a887d8e1641
rajlinksture <116059894+rajlinksture@users.noreply.github.com>
2023-06-27 17:56:15 +0530

    Update centred content image gallery block

--- wp-content/themes/timefinance/template-parts/acf-flexible/centred-content-image-gallery.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/centred-content-image-gallery.php
@@ -13,26 +13,30 @@ $section_id    = get_sub_field( 'section_id' ) ? get_sub_field( 'section_id' ) :
 ?>
 <section class="centred-content-image-gallery" id="<?php echo $section_id; //phpcs:ignore ?>">
 	<div class="container">
-		<div class="row">
+		<div class="row justify-content-center text-center">
 			<div class="col">
 				<?php
 				if ( ! empty( $main_title ) ) {
-					echo '<' . esc_attr( $select_tag ) . ' class="section-title">' . esc_html( $main_title ) . '</' . esc_attr( $select_tag ) . '>';
+					echo '<' . esc_attr( $select_tag ) . ' class="section-title h-4">' . esc_html( $main_title ) . '</' . esc_attr( $select_tag ) . '>';
 				}
 				echo $content; //phpcs:ignore
 				?>
 			</div>
 		</div>
-		<div class="row">
-			<div class="col">
-				<?php
-				foreach ( $image_gallary as $image ) {
-					$image_alt = ( isset( $image['alt'] ) && ! empty( $image['alt'] ) ) ? $image['alt'] : ( isset( $image['title'] ) && ! empty( $image['title'] ) ? $image['title'] : '' );
-					?>
-					<img class="image-gallery" width="<?php echo $image['sizes']['timefinance-desktop-width']; ?>" height="<?php echo 	$image['sizes']['timefinance-desktop-height']; ?>" src="<?php echo $image['url']; ?>" srcset="<?php echo $image['sizes']['timefinance-small-mobile']; ?> 400w, <?php echo $image['sizes']['timefinance-mobile']; ?> 800w, <?php echo $image['sizes']['timefinance-tablet']; ?> 1200w, <?php echo $image['sizes']['timefinance-desktop']; ?> 2000w" sizes="50vw" alt="<?php echo $image_alt; //phpcs:ignore ?>">
+		<div class="row justify-content-center">
+			<div class="col-lg-10">
+				<div class="row row-cols-1 row-cols-lg-4 justify-content-center align-items-center text-center">
 					<?php
-				}
-				?>
+					foreach ( $image_gallary as $image ) {
+						$image_alt = ( isset( $image['alt'] ) && ! empty( $image['alt'] ) ) ? $image['alt'] : ( isset( $image['title'] ) && ! empty( $image['title'] ) ? $image['title'] : '' );
+						?>
+						<div class="col logo">
+							<img class="image-gallery" width="<?php echo $image['sizes']['timefinance-desktop-width']; ?>" height="<?php echo 	$image['sizes']['timefinance-desktop-height']; ?>" src="<?php echo $image['url']; ?>" srcset="<?php echo $image['sizes']['timefinance-small-mobile']; ?> 400w, <?php echo $image['sizes']['timefinance-mobile']; ?> 800w, <?php echo $image['sizes']['timefinance-tablet']; ?> 1200w, <?php echo $image['sizes']['timefinance-desktop']; ?> 2000w" sizes="50vw" alt="<?php echo $image_alt; //phpcs:ignore ?>">
+						</div>
+						<?php
+					}
+					?>
+				</div>
 			</div>
 		</div>
 	</div>
