2f0d18b888a7d256f2d03032d8293d2978bed2b7
rajlinksture <116059894+rajlinksture@users.noreply.github.com>
2023-06-27 11:35:32 +0530

    Update inner page hero block

--- wp-content/themes/timefinance/template-parts/acf-flexible/inner-page-hero.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/inner-page-hero.php
@@ -12,20 +12,19 @@ $background_image = get_sub_field( 'background_image' );
 $cta_button       = get_sub_field( 'cta_button' );
 $left_image      = get_sub_field( 'left_image' );
 $background       = ( ! empty( $background_image ) ) ? ' style="background-image:url(' . esc_url( $background_image ) . ');"' : '';
-$left_image_alt  = ( isset( $left_image['alt'] ) && ! empty( $left_image['alt'] ) ) ? $left_image['alt'] : ( isset( $left_image['title'] ) && ! empty( $left_image['title'] ) ? $left_image['title'] : '' );
+$left_image_background       = ( ! empty( $left_image ) ) ? ' style="background-image:url(' . esc_url( $left_image ) . ');"' : '';
 $section_id       = get_sub_field( 'section_id' ) ? get_sub_field( 'section_id' ) : uniqid( 'inner-page-hero-' );
 
 ?>
 <section class="inner-page-hero" id="<?php echo $section_id; //phpcs:ignore ?>"<?php echo $background; ?>>
-	<div class="section-left-image">
-		<img class="left-image" width="<?php echo $left_image['sizes']['timefinance-desktop-width']; ?>" height="<?php echo 	$left_image['sizes']['timefinance-desktop-height']; ?>" src="<?php echo $left_image['url']; ?>" srcset="<?php echo $left_image['sizes']['timefinance-small-mobile']; ?> 400w, <?php echo $left_image['sizes']['timefinance-mobile']; ?> 800w, <?php echo $left_image['sizes']['timefinance-tablet']; ?> 1200w, <?php echo $left_image['sizes']['timefinance-desktop']; ?> 2000w" sizes="50vw" alt="<?php echo $left_image_alt; //phpcs:ignore ?>">
+	<div class="section-left-image" <?php echo $left_image_background; ?>>
 	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col">
 				<?php
 				if ( ! empty( $main_title ) ) {
-					echo '<' . esc_attr( $select_tag ) . ' class="section-title">' . esc_html( $main_title ) . '</' . esc_attr( $select_tag ) . '>';
+					echo '<' . esc_attr( $select_tag ) . ' class="section-title h-2">' . esc_html( $main_title ) . '</' . esc_attr( $select_tag ) . '>';
 				}
 				echo $content; //phpcs:ignore
 				if ( $cta_button && ! empty( $cta_button['url'] ) && ! empty( $cta_button['title'] ) ) {
