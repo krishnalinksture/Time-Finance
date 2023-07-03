d8ac8c84fbfb99bb9f9b05925e6727a4224f12f9
rajlinksture <116059894+rajlinksture@users.noreply.github.com>
2023-07-03 10:18:16 +0530

    Update our-history-block.php

--- wp-content/themes/timefinance/template-parts/acf-flexible/our-history-block.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/our-history-block.php
@@ -24,39 +24,42 @@ $section_id      = get_sub_field( 'section_id' ) ? get_sub_field( 'section_id' )
 		<?php
 		if ( have_rows( 'history_content' ) ) {
 			?>
-			<div class="separator">
-				<?php
-				while ( have_rows( 'history_content' ) ) {
-					the_row();
-					$image           = get_sub_field( 'image' );
-					$date            = get_sub_field( 'date' );
-					$history_title   = get_sub_field( 'title' );
-					$select_position = get_sub_field( 'select_position' );
-					$position        = ( 'right' === $select_position ) ? 'justify-content-end' : '';
-					$image_alt       = ( isset( $image['alt'] ) && ! empty( $image['alt'] ) ) ? $image['alt'] : ( isset( $image['title'] ) && ! empty( $image['title'] ) ? $image['title'] : '' );
-					?>
-					<div class="row <?php echo $position; //phpcs:ignore ?>">
-						<div class="col-12 col-md-5">
-							<div class="history-card">
-								<img width="<?php echo $image['sizes']['timefinance-desktop-width']; ?>" height="<?php echo $image['sizes']['timefinance-desktop-height']; ?>" src="<?php echo $image['url']; ?>" srcset="<?php echo $image['sizes']['timefinance-small-mobile']; ?> 400w, <?php echo $image['sizes']['timefinance-mobile']; ?> 800w, <?php echo $image['sizes']['timefinance-tablet']; ?> 1200w, <?php echo $image['sizes']['timefinance-desktop']; ?> 2000w" sizes="50vw" alt="<?php echo $image_alt; //phpcs:ignore ?>">
-								<div class="card-content">
-									<div class="card-title">
-										<?php
-										if ( ! empty( $date ) ) {
-											echo esc_html( $date );
-										}
-										?>
-									</div>
-									<div class="card-text">
-										<?php echo esc_html( $history_title ); ?>
+			<div class="row justify-content-center">
+				<div class="col-11">
+					<div class="separator"></div>
+					<?php
+					while ( have_rows( 'history_content' ) ) {
+						the_row();
+						$image           = get_sub_field( 'image' );
+						$date            = get_sub_field( 'date' );
+						$history_title   = get_sub_field( 'title' );
+						$select_position = get_sub_field( 'select_position' );
+						$position        = ( 'right' === $select_position ) ? 'justify-content-end' : '';
+						$image_alt       = ( isset( $image['alt'] ) && ! empty( $image['alt'] ) ) ? $image['alt'] : ( isset( $image['title'] ) && ! empty( $image['title'] ) ? $image['title'] : '' );
+						?>
+						<div class="row <?php echo $position; //phpcs:ignore ?>">
+							<div class="col-12 col-md-5">
+								<div class="history-card">
+									<img width="<?php echo $image['sizes']['timefinance-desktop-width']; ?>" height="<?php echo $image['sizes']['timefinance-desktop-height']; ?>" src="<?php echo $image['url']; ?>" srcset="<?php echo $image['sizes']['timefinance-small-mobile']; ?> 400w, <?php echo $image['sizes']['timefinance-mobile']; ?> 800w, <?php echo $image['sizes']['timefinance-tablet']; ?> 1200w, <?php echo $image['sizes']['timefinance-desktop']; ?> 2000w" sizes="50vw" alt="<?php echo $image_alt; //phpcs:ignore ?>">
+									<div class="card-content">
+										<div class="card-title">
+											<?php
+											if ( ! empty( $date ) ) {
+												echo esc_html( $date );
+											}
+											?>
+										</div>
+										<div class="card-text">
+											<?php echo esc_html( $history_title ); ?>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<?php
-				}
-				?>
+						<?php
+					}
+					?>
+				</div>
 			</div>
 			<?php
 		}
