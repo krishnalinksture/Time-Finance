ae9db61dea865bf7d82e1522535051cfdcdeae22
rajlinksture <116059894+rajlinksture@users.noreply.github.com>
2023-07-04 12:01:24 +0530

    Update calculator block

--- wp-content/themes/timefinance/template-parts/acf-flexible/calculator-block.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/calculator-block.php
@@ -16,26 +16,28 @@ $section_id     = get_sub_field( 'section_id' ) ? get_sub_field( 'section_id' ) 
 <section class="calculator-block" id="<?php echo $section_id; //phpcs:ignore ?>">
 	<div class="container">
 		<div class="row justify-content-center text-center">
-			<div class="col content-wrapper">
-				<?php
-				if ( ! empty( $main_title ) ) {
-					echo '<' . esc_attr( $select_tag ) . ' class="section-title h-4">' . esc_html( $main_title ) . '</' . esc_attr( $select_tag ) . '>';
-				}
-				echo $content; //phpcs:ignore
-				?>
-				<div class="value-title">
-					<?php echo esc_html( $value_title ); ?>
-				</div>
-				<form>
-					<input type="text" id="amount_value" name="amount_value" value="10,000">
-					<button class="btn submit">submit</button>
-				</form>
-				<div class="result-title">
-					<?php echo esc_html( $result_title ); ?>
-				</div>
-				<div class="result">£9,000*</div>
-				<div class="approval-title">
-					<?php echo esc_html( $approval_title ); ?>
+			<div class="col calculator-wrapper">
+				<div class="content">
+					<?php
+					if ( ! empty( $main_title ) ) {
+						echo '<' . esc_attr( $select_tag ) . ' class="section-title h-4">' . esc_html( $main_title ) . '</' . esc_attr( $select_tag ) . '>';
+					}
+					echo $content; //phpcs:ignore
+					?>
+					<div class="value-title">
+						<?php echo esc_html( $value_title ); ?>
+					</div>
+					<form>
+						<input type="text" id="amount_value" name="amount_value" value="10,000">
+						<button class="btn submit">submit</button>
+					</form>
+					<div class="result-title">
+						<?php echo esc_html( $result_title ); ?>
+					</div>
+					<div class="result">£9,000*</div>
+					<div class="approval-title">
+						<?php echo esc_html( $approval_title ); ?>
+					</div>
 				</div>
 			</div>
 		</div>
