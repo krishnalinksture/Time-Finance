689ef2f2bc748ca085beb7f712c3327479e1b5e9
krishnalinksture <105272076+krishnalinksture@users.noreply.github.com>
2023-07-10 13:36:36 +0530

    some changes

--- wp-content/themes/timefinance/template-parts/acf-flexible/content-block.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/content-block.php
@@ -6,13 +6,14 @@
  */
 
 $content    = get_sub_field( 'content' );
+$select_text_align    = get_sub_field( 'select_text_align' );
 $section_id = get_sub_field( 'section_id' ) ? get_sub_field( 'section_id' ) : uniqid( 'content-block-' );
 
 if ( ! empty( $content ) ) {
 	?>
 	<section class="content-block" id="<?php echo $section_id; //phpcs:ignore ?>">
 		<div class="container">
-			<div class="row justify-content-center text-center">
+			<div class="row justify-content-center <?php echo $select_text_align; //phpcs:ignore ?>">
 				<div class="col">
 					<?php echo $content; //phpcs:ignore ?>
 				</div>
--- wp-content/themes/timefinance/template-parts/acf-flexible/resources-block.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/resources-block.php
@@ -132,6 +132,10 @@ if ( ! empty( $main_title ) || ! empty( $content ) ) {
 												<div class="modal fade" id="resourseModal" tabindex="-1" aria-labelledby="resourseModalLabel" aria-hidden="true">
 													<div class="modal-dialog">
 														<div class="modal-content">
+															<div class="modal-header">
+																<h5 class="modal-title" id="bdtModalLabel"><?php echo esc_html( 'Your Download Request' ); ?></h5>
+																<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+															</div>
 															<div class="modal-body">
 																<div class="row justify-content-center">
 																	<div class="col">
