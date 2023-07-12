07ec3140737d782bfa62584ddc19fb5a29f6ee82
krishnalinksture <105272076+krishnalinksture@users.noreply.github.com>
2023-07-12 18:35:42 +0530

    Update inner-page-hero.php

--- wp-content/themes/timefinance/template-parts/acf-flexible/inner-page-hero.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/inner-page-hero.php
@@ -11,14 +11,17 @@ $content               = get_sub_field( 'content' );
 $background_image      = get_sub_field( 'background_image' );
 $cta_button            = get_sub_field( 'cta_button' );
 $left_image            = get_sub_field( 'left_image' );
-$background            = ( ! empty( $background_image ) ) ? ' style="background-image:url(' . esc_url( $background_image ) . ');"' : '';
+$background_image_alt = ( isset( $background_image['alt'] ) && ! empty( $background_image['alt'] ) ) ? $background_image['alt'] : ( isset( $background_image['title'] ) && ! empty( $background_image['title'] ) ? $background_image['title'] : '' );
 $left_image_background = ( ! empty( $left_image ) ) ? ' style="background-image:url(' . esc_url( $left_image ) . ');"' : '';
 $section_id            = get_sub_field( 'section_id' ) ? get_sub_field( 'section_id' ) : uniqid( 'inner-page-hero-' );
 
 if ( ! empty( $main_title ) || ! empty( $content ) || ! empty( $cta_button ) ) {
 	?>
-	<section class="inner-page-hero" id="<?php echo $section_id; //phpcs:ignore ?>"<?php echo $background; ?>>
+	<section class="inner-page-hero" id="<?php echo $section_id; //phpcs:ignore ?>">
 		<div class="section-left-image" <?php echo $left_image_background; //phpcs:ignore ?>></div>
+		<div class="section-right-image">
+			<img class="right-image" width="<?php echo $background_image['sizes']['timefinance-desktop-width']; ?>" height="<?php echo $background_image['sizes']['timefinance-desktop-height']; ?>" src="<?php echo $background_image['url']; ?>" srcset="<?php echo $background_image['sizes']['timefinance-small-mobile']; ?> 400w, <?php echo $background_image['sizes']['timefinance-mobile']; ?> 800w, <?php echo $background_image['sizes']['timefinance-tablet']; ?> 1200w, <?php echo $background_image['sizes']['timefinance-desktop']; ?> 2000w" sizes="50vw" alt="<?php echo $background_image_alt; //phpcs:ignore ?>">
+		</div>
 		<div class="container">
 			<div class="row justify-content-center align-items-center">
 				<div class="col">
