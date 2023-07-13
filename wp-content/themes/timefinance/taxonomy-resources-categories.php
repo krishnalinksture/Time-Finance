9a07dba2f22742bff49da4a20243735708c23370
krishnalinksture <105272076+krishnalinksture@users.noreply.github.com>
2023-07-13 19:12:45 +0530

    Update taxonomy-resources-categories.php

--- wp-content/themes/timefinance/taxonomy-resources-categories.php
+++ wp-content/themes/timefinance/taxonomy-resources-categories.php
@@ -124,6 +124,10 @@ $resources_form            = ( ! empty( $resources_select_form ) ) ? '[forminato
 											<div class="modal fade" id="resourcesModal" tabindex="-1" aria-label="resourcesModalLabel" aria-hidden="true">
 												<div class="modal-dialog">
 													<div class="modal-content">
+														<div class="modal-header">
+															<h5 class="modal-title" id="bdtModalLabel"><?php echo esc_html( 'Your Download Request' ); ?></h5>
+															<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+														</div>
 														<?php
 														if ( ! empty( $resources_select_form ) ) {
 															?>
