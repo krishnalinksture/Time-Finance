71fe81b783106303026595e97cf01712110763be
rajlinksture <116059894+rajlinksture@users.noreply.github.com>
2023-07-14 17:02:47 +0530

    Some changes

--- wp-content/themes/timefinance/template-parts/acf-flexible/office-locations-block.php
+++ wp-content/themes/timefinance/template-parts/acf-flexible/office-locations-block.php
@@ -27,7 +27,7 @@ if ( ! empty( $main_title ) || have_rows( 'location_cards' ) ) {
 			}
 			if ( have_rows( 'location_cards' ) ) {
 				?>
-				<div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 justify-content-center">
+				<div class="row row-cols-1 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 justify-content-center">
 					<?php
 					while ( have_rows( 'location_cards' ) ) {
 						the_row();
