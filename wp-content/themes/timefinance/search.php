84c417cbecb08dd4464039c87621c711348f8a6c
krishnalinksture <105272076+krishnalinksture@users.noreply.github.com>
2023-07-19 15:29:06 +0530

    Update search.php

--- wp-content/themes/timefinance/search.php
+++ wp-content/themes/timefinance/search.php
@@ -49,85 +49,81 @@ if ( ! empty( $banner_title ) || ! empty( $search_again ) || ! empty( $find_out_
 	</section>
 	<?php
 }
-if ( have_posts() ) {
-	?>
-	<section class="search-block">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-lg-9 search-wrapper">
-					<div class="search-box">
-						<form id="search-again" method="get" action="<?php echo home_url('/'); //phpcs:ignore ?>" name="search-header" class="search-form-result">
-							<input type="text" name="s" class="form-control" placeholder="Search again..." aria-label="Search again" aria-describedby="basic-addon2" autocomplete="off">
-							<div class="input-group-append">
-								<button class="btn btn-green" type="submit">
-									<?php echo esc_html( $search_again ); ?>
-								</button>
-							</div>
-						</form>
-					</div>
+?>
+<section class="search-block">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-9 search-wrapper">
+				<div class="search-box">
+					<form id="search-again" method="get" action="<?php echo home_url('/'); //phpcs:ignore ?>" name="search-header" class="search-form-result">
+						<input type="text" name="s" class="form-control" placeholder="Search again..." aria-label="Search again" aria-describedby="basic-addon2" autocomplete="off">
+						<div class="input-group-append">
+							<button class="btn btn-green" type="submit">
+								<?php echo esc_html( $search_again ); ?>
+							</button>
+						</div>
+					</form>
 				</div>
 			</div>
-			<?php
-			if ( have_posts() ) {
-				?>
-				<div class="row justify-content-center">
-					<div class="col-lg-9 search-wrapper">
-						<?php
-						while ( have_posts() ) {
-							the_post();
-							?>
-							<div class="search-data">
+		</div>
+		<?php
+		if ( have_posts() ) {
+			?>
+			<div class="row justify-content-center">
+				<div class="col-lg-9 search-wrapper">
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						?>
+						<div class="search-data">
+							<?php
+							if ( get_the_title() ) {
+								?>
+								<div class="search-title">
+									<?php echo get_the_title(); //phpcs:ignore?>
+								</div>
 								<?php
-								if ( get_the_title() ) {
-									?>
-									<div class="search-title">
-										<?php echo get_the_title(); //phpcs:ignore?>
-									</div>
+							}
+							$_term = get_queried_object();
+							if ( get_the_excerpt() || have_rows( 'page_builder', $_term ) ) {
+								?>
+								<div class="search-content">
 									<?php
-								}
-								$_term = get_queried_object();
-								if ( get_the_excerpt() || have_rows( 'page_builder', $_term ) ) {
-									?>
-									<div class="search-content">
-										<?php
-										if ( get_the_excerpt() ) {
-											echo wp_trim_words( get_the_excerpt(), 19, ' ' ); //phpcs:ignore
-										} else {
-											if ( have_rows( 'page_builder', $_term ) ) {
-												while ( have_rows( 'page_builder', $_term ) ) {
-													the_row();
-													if ( get_row_layout() === 'trustpilot_block' ) {
-														$content = get_sub_field( 'content' );
-														echo wp_trim_words( $content, 19, ' ' ); //phpcs:ignore
-													}
+									if ( get_the_excerpt() ) {
+										echo wp_trim_words( get_the_excerpt(), 19, ' ' ); //phpcs:ignore
+									} else {
+										if ( have_rows( 'page_builder', $_term ) ) {
+											while ( have_rows( 'page_builder', $_term ) ) {
+												the_row();
+												if ( get_row_layout() === 'trustpilot_block' ) {
+													$content = get_sub_field( 'content' );
+													echo wp_trim_words( $content, 19, ' ' ); //phpcs:ignore
 												}
 											}
 										}
-										?>
-									</div>
-									<?php
-								}
-								if ( ! empty( $find_out_more_button ) ) {
+									}
 									?>
-									<div class="search-read-more">
-										<?php $page_url = get_permalink(); ?>
-										<a href="<?php echo esc_url( $page_url ); ?>"><button class="btn btn-green"><?php echo esc_html( $find_out_more_button ); ?></button></a>
-									</div>
-									<?php
-								}
+								</div>
+								<?php
+							}
+							if ( ! empty( $find_out_more_button ) ) {
 								?>
-							</div>
-							<?php
-							wp_reset_postdata();
-						}
-						?>
-					</div>
+								<div class="search-read-more">
+									<?php $page_url = get_permalink(); ?>
+									<a href="<?php echo esc_url( $page_url ); ?>"><button class="btn btn-green"><?php echo esc_html( $find_out_more_button ); ?></button></a>
+								</div>
+								<?php
+							}
+							?>
+						</div>
+						<?php
+						wp_reset_postdata();
+					}
+					?>
 				</div>
-				<?php
-			}
-			?>
-		</div>
-	</section>
-	<?php
-}
-
+			</div>
+			<?php
+		}
+		?>
+	</div>
+</section>
